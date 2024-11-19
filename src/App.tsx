1b5ff40c77917194942dfee7b88dd174e1e56515
OscarLTC <oscartazacordero@gmail.com>
2024-11-19 17:52:49 -0500

    [custom hooks]

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,26 @@
-import { useCallback, useEffect, useState } from "react";
-import "./App.css";
+import "./App.css"
+import { useFetch } from "./hooks"
+
+const url = 'https://api.example.com/data'
+
+interface Data {
+  name: string
+  lastName: string
+  age: number
+}
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
 
-  const consolLoader = useCallback(
-    (loadingValue: boolean) => {
-      setLoading(loadingValue);
-      console.info(loading);
-    },
-    [loading]
-  );
-
-  const fetchData = useCallback(async () => {
-    consolLoader(true);
-    try {
-      const response = await fetch("https://api.example.com/data");
-
-      if (!response.ok) {
-        throw new Error("Error al obtener datos");
-      }
-      const jsonData = await response.json();
-
-      setData(jsonData);
-    } catch (error) {
-      setError(error as string);
-    } finally {
-      consolLoader(false);
-    }
-  }, [consolLoader]);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+  const { data, error, loading } = useFetch<Data>(url)
 
   if (loading) {
-    return <div>Cargando...</div>;
+    return <div>Cargando...</div>
   }
 
   if (error) {
-    return <div>UPS! Hay un error: {error}</div>;
+    return <div>UPS! Hay un error: {error.message}</div>
   }
 
-  return <div>{JSON.stringify(data)}</div>;
+  return <div>{JSON.stringify(data)}</div>
 }
-export default App;
+export default App
