74b05efe4140a4a8c27d831fb637068550fc8332
OscarLTC <oscartazacordero@gmail.com>
2024-11-18 21:30:31 -0500

    [useEffect]

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
-import { Button } from "./components";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const countMore = () => {
-    setCount((count) => count + 1);
-  };
-
-  return (
-    <>
-      <Button label={`Count is ${count}`} parentMethod={countMore} />
-    </>
+  const consolLoader = useCallback(
+    (loadingValue: boolean) => {
+      setLoading(loadingValue);
+      console.info(loading);
+    },
+    [loading]
   );
-}
 
+  const fetchData = useCallback(async () => {
+    consolLoader(true);
+    try {
+      const response = await fetch("https://api.example.com/data");
+
+      if (!response.ok) {
+        throw new Error("Error al obtener datos");
+      }
+      const jsonData = await response.json();
+
+      setData(jsonData);
+    } catch (error) {
+      setError(error as string);
+    } finally {
+      consolLoader(false);
+    }
+  }, [consolLoader]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (error) {
+    return <div>UPS! Hay un error: {error}</div>;
+  }
+
+  return <div>{JSON.stringify(data)}</div>;
+}
 export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
+
 interface Props {
   label: string;
   parentMethod: () => void;
 }
 
 export const Button = ({ label, parentMethod }: Props) => {
+  useEffect(() => {
+    console.log("label cambio");
+  }, [label]);
+
   return <button onClick={parentMethod}>{label}</button>;
 };
